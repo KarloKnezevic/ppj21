9084408fd6b081fe930c17ad2fb282366662e36d
tin.franovic@gmail.com <tin.franovic@gmail.com@23cb9226-ac5d-11de-8ba5-91705c1537ed>
2009-09-28 19:38:46 +0000

    Ovdje ocekujem da i vi pustate komentare kad uploadate nesto kako bi se znalo tko je sto mijenjao.. ;)

---
+++ src/hr/fer/ppj21/welcome/Welcome.java
@@ -0,0 +1,14 @@
+package hr.fer.ppj21.welcome;
+
+public class Welcome {
+
+	/*
+	 * Ako ovo èitate, onda je sve ok.. ;)
+	 */
+	public static void main(String[] args) {
+		System.out.println("Dobrodosli!");
+		System.out.println("Ovo je SVN repozitorij u kojem cemo imati sve source kodove");
+
+	}
+
+}

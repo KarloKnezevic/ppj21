de6292a7febc03e28c1d236727330d5a71569fb1
karabaja4@gmail.com <karabaja4@gmail.com@23cb9226-ac5d-11de-8ba5-91705c1537ed>
2009-09-28 21:51:26 +0000

    dodane klase za probu

---
+++ src/hr/fer/ppj21/igor_saric/zadaci1/Zadaci1_01.java
@@ -0,0 +1,13 @@
+package hr.fer.ppj21.igor_saric.zadaci1;
+
+public class Zadaci1_01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/hr/fer/ppj21/igor_saric/zadaci2/Zadaci2_01.java
@@ -0,0 +1,13 @@
+package hr.fer.ppj21.igor_saric.zadaci2;
+
+public class Zadaci2_01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

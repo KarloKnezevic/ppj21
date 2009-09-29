97853862c93ba100e770aef6b2335f6090b5aeb5
karabaja4@gmail.com <karabaja4@gmail.com@23cb9226-ac5d-11de-8ba5-91705c1537ed>
2009-09-29 02:16:41 +0000

    Riješen zadatak 1.3

---
+++ src/hr/fer/ppj21/igor_saric/zadaci1/Zadaci1_03.java
@@ -0,0 +1,55 @@
+package hr.fer.ppj21.igor_saric.zadaci1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+* @author Igor Saric
+* @version 1.0
+*/
+
+public class Zadaci1_03 {
+	
+	private static int najmanji(int[] polje) {
+		
+		int br_elem = polje.length;
+		
+		/* najmanji postavimo kao prvi element, nije bitno */
+		int min = polje[0];
+		
+		/* nadji najmanji element */
+		for(int i = 0; i < br_elem; i++) {
+			if (polje[i] < min) min = polje[i];
+		}
+		
+		/* vrati najmanji */
+		return min;
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		/* unos broja elemenata */
+		System.out.print("Unesi broj elemenata polja: ");
+		String unos = reader.readLine();
+		int br_elem = Integer.parseInt(unos);
+		
+		/* kreiraj int polje trazene velicine */
+		int[] polje = new int[br_elem];
+		
+		/* unos elemenata */
+		for(int i = 0; i < br_elem; i++) {
+			
+			System.out.print("Unesite " + (i + 1) + ". element: ");
+			unos = reader.readLine();
+			polje[i] = Integer.parseInt(unos);
+
+		}
+		
+		/*ispis najmanjeg */
+		System.out.println("Najmanji broj je " + najmanji(polje) + ".");
+	}
+}
